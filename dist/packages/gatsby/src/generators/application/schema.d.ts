941793b909e81aeffbbd8cc8958e3139628e2a45
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2023-06-14 09:03:19 -0400

    chore: nx commands

---
+++ dist/packages/gatsby/src/generators/application/schema.d.ts
@@ -0,0 +1,13 @@
+import { SupportedStyles } from '@nx/react'
+
+export interface Schema {
+  name: string
+  tags?: string
+  style: SupportedStyles
+  directory?: string
+  unitTestRunner?: 'jest' | 'none'
+  e2eTestRunner?: 'cypress' | 'none'
+  js?: boolean
+  setParserOptionsProject?: boolean
+  standaloneConfig?: boolean
+}
---
+++ dist/packages/gatsby/src/generators/init/init.spec.ts
@@ -0,0 +1,33 @@
+import { NxJsonConfiguration, readJson, Tree } from '@nx/devkit'
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing'
+
+import { gatsbyInitGenerator } from './init'
+
+xdescribe('init', () => {
+  let tree: Tree
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace()
+  })
+
+  it('should add react dependencies', async () => {
+    await gatsbyInitGenerator(tree, {})
+    const packageJson = readJson(tree, 'package.json')
+    expect(packageJson.dependencies['@nx/gatsby']).toBeUndefined()
+    expect(packageJson.dependencies['@nx/react']).toBeUndefined()
+    expect(packageJson.dependencies['gatsby']).toBeDefined()
+  })
+
+  describe('defaultCollection', () => {
+    it('should be set if none was set before', async () => {
+      await gatsbyInitGenerator(tree, {})
+      const { cli } = readJson<NxJsonConfiguration>(tree, 'nx.json')
+      expect(cli.defaultCollection).toEqual('@nx/gatsby')
+    })
+  })
+
+  it('should not add jest config if unitTestRunner is none', async () => {
+    await gatsbyInitGenerator(tree, { unitTestRunner: 'none' })
+    expect(tree.exists('jest.config.js')).toEqual(false)
+  })
+})
