a81ef7afa2b764151c10c2280766b31702d807fa
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2023-06-14 10:40:32 -0400

    test: add jest to gatsby

--- packages/gatsby/jest.config.ts
+++ packages/gatsby/jest.config.ts
@@ -1,11 +1,6 @@
 /* eslint-disable */
 export default {
-  transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  globals: {},
   displayName: 'gatsby',
-  testEnvironment: 'node',
   preset: '../../jest.preset.js',
+  coverageDirectory: '../../coverage/packages/gatsby',
 }
