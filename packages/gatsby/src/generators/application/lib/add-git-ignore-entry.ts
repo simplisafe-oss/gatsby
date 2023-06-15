b0f29d808a8e3c58ff0a8ea56c32e12de90bbc0e
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2023-06-15 07:33:53 -0400

    fix: use correct path for prettierignore and gitignore files

--- packages/gatsby/src/generators/application/lib/add-git-ignore-entry.ts
+++ packages/gatsby/src/generators/application/lib/add-git-ignore-entry.ts
@@ -4,7 +4,7 @@ import { NormalizedSchema } from './normalize-options'
 export function addGitIgnoreEntry(host: Tree, options: NormalizedSchema) {
   if (host.exists('.gitignore')) {
     let content = host.read('.gitignore', 'utf-8')
-    content = `${content}\n/${options.projectRoot}/${options.projectName}/node_modules\n/${options.projectRoot}/${options.projectName}/public\n/${options.projectRoot}/${options.projectName}/.cache\n`
+    content = `${content}\n/${options.projectRoot}/node_modules\n/${options.projectRoot}/public\n/${options.projectRoot}/.cache\n`
     host.write('.gitignore', content)
   } else {
     logger.warn(`Couldn't find .gitignore file to update`)
--- packages/gatsby/src/generators/application/lib/add-prettier-ignore-entry.ts
+++ packages/gatsby/src/generators/application/lib/add-prettier-ignore-entry.ts
@@ -2,9 +2,10 @@ import { logger, Tree } from '@nx/devkit'
 import { NormalizedSchema } from './normalize-options'
 
 export function addPrettierIgnoreEntry(host: Tree, options: NormalizedSchema) {
+  console.log(options)
   if (host.exists('.prettierignore')) {
     let content = host.read('.prettierignore', 'utf-8')
-    content = `${content}\n/${options.projectDirectory}/${options.projectName}/node_modules\n/${options.projectDirectory}/${options.projectName}/public\n/${options.projectDirectory}/${options.projectName}/.cache\n`
+    content = `${content}\n/${options.projectRoot}/node_modules\n/${options.projectRoot}/public\n/${options.projectRoot}/.cache\n`
     host.write('.prettierignore', content)
   } else {
     logger.warn(`Couldn't find .prettierignore file to update`)
